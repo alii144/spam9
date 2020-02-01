7f42976100a2d0baef5a093fdc899ac4ed83b618
alii144 <56957681+alii144@users.noreply.github.com>
2020-02-01 09:04:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By snow_ghost");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("id الغرفة")
+let channel =     client.channels.get("672453658339835954")
 setInterval(function() {
 channel.send(`هلا هلا هلا هلا هلا`);
 }, 30)
